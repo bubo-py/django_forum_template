cb6b5aa0253497efb2719ca0315529b9c5f0b80a
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-05 23:46:31 +0100

    created simple routes for each thread

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:thread_id>/', views.thread_detail, name='detail')
 ]
