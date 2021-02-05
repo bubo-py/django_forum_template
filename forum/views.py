0d52307986dc9e2d915f415453bbb36d7269e1e9
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-05 23:47:07 +0100

    created simple view for detailed thread

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+# from .models import ForumThread
 
 
 def index(request):
     return HttpResponse("it's an index page")
+
+
+def thread_detail(request, thread_id):
+    return HttpResponse(f"this is thread number {thread_id}")
