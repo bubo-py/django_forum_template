36a95f5fa75371688d4c302f1833bf7313eed7e8
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-05 23:45:50 +0100

    added dunder str method

--- forum/models.py
+++ forum/models.py
@@ -8,3 +8,6 @@ class ForumThread(models.Model):
     thread_content = models.TextField(max_length=750)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.thread_heading
