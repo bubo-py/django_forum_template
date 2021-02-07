9c694c546710bbd026dad7dc89c53df917b0ade4
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-07 20:23:42 +0100

    changed field names for more convenient

--- forum/models.py
+++ forum/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 
 class ForumThread(models.Model):
-    thread_heading = models.CharField(max_length=100)
-    thread_content = models.TextField(max_length=750)
+    heading = models.CharField(max_length=100)
+    content = models.TextField(max_length=750)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
