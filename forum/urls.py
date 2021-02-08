9f50022d123d6288c660cfa554a2b6c1cbcec46c
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-08 23:22:40 +0100

    added url for creating thread

--- forum/urls.py
+++ forum/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:thread_id>/', views.thread_detail, name='detail')
+    path('<int:thread_id>/', views.thread_detail, name='detail'),
+    path('add/', views.add_thread, name='add')
 ]
