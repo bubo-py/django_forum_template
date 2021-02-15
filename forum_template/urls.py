534a26e0a40dea8ef2d750a2efb9c8ed50e6084c
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-15 21:36:00 +0100

    added route for register page

--- forum_template/urls.py
+++ forum_template/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from forum import views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('forum.urls'))
+    path('', include('forum.urls')),
+    path('register/', users_views.register, name='register')
 ]
