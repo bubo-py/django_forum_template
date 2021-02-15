6b63263aa2117d531407ad67a4ac55b08cdb6919
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-15 21:36:49 +0100

    added users app with views and templates

---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            new_user = form.cleaned_data.get('username')
+            messages.success(request, f"created account for {new_user}")
+            return redirect('index')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
