42c9e8394165c6d7e5863203162bcf4ac5391ba8
bubo-py <kamil.chruscinski1@gmail.com>
2021-02-20 18:31:30 +0100

    added route for password reset

--- forum_template/urls.py
+++ forum_template/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('forum.urls')),
     path('register/', users_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='reset')
 ]
