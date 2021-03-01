ff706f8473fe0ca1cb56377477021e230906cd9e
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-01 21:46:30 +0100

    changed index and detail view to class based views

--- forum/views.py
+++ forum/views.py
@@ -2,17 +2,31 @@ from .models import ForumThread
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import AddThreadForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView
 
 
-def index(request):
-    template = 'forum/index.html'
-    threads = ForumThread.objects.all().order_by('-date_posted')[:5]
-    return render(request, template, {'threads': threads})
+# def index(request):
+#     template = 'forum/index.html'
+#     threads = ForumThread.objects.all().order_by('-date_posted')[:5]
+#     return render(request, template, {'threads': threads})
 
 
-def thread_detail(request, thread_id):
-    thread = get_object_or_404(ForumThread, pk=thread_id)
-    return render(request, 'forum/thread_detail.html', {'thread': thread})
+# def thread_detail(request, thread_id):
+#     thread = get_object_or_404(ForumThread, pk=thread_id)
+#     return render(request, 'forum/thread_detail.html', {'thread': thread})
+
+
+class ThreadListView(ListView):
+    model = ForumThread
+    template_name = 'forum/index.html'
+    context_object_name = 'threads'
+    ordering = '-date_posted'
+    paginate_by = 5
+
+
+class ThreadDetailView(DetailView):
+    model = ForumThread
+    context_object_name = 'thread'
 
 
 @login_required
