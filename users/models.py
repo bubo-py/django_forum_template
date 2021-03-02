a2e9f06cec5bd321d0bdb9e5e464a927e67a1fd4
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-02 22:13:42 +0100

    added user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    user_avatar = models.ImageField(upload_to='user_avatars')
+
+    def __str__(self):
+        return self.user.username
