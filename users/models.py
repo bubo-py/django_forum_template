ae377831cacaa15f62448633a2d4b3e94db123c8
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-03 14:54:54 +0100

    added default avatar in case of not uploading any

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    user_avatar = models.ImageField(upload_to='user_avatars')
+    user_avatar = models.ImageField(upload_to='user_avatars', default='default_avatar.png')
 
     def __str__(self):
         return self.user.username
