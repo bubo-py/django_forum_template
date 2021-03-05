caacf1bf528649f3dc579474df6136aaf86995f9
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-05 13:00:28 +0100

    added route for OneUserThreads view

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ThreadListView, ThreadDetailView
+from .views import ThreadListView, ThreadDetailView, OneUserThreadsView
 from . import views
 
 
 urlpatterns = [
     path('', ThreadListView.as_view(), name='index'),
     path('thread/<int:pk>/', ThreadDetailView.as_view(), name='detail'),
+    path('user/<str:username>/threads/', OneUserThreadsView.as_view(), name='one-threads-list'),
     path('add/', views.add_thread, name='add')
 ]
