74ae9b5bda5e6b58bfc4916d13cc25263db63754
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-05 16:40:49 +0100

    added auto-resizing the profile picture

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -11,3 +12,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save()
+        image_path = self.user_avatar.path
+
+        i = Image.open(image_path)
+
+        if i.width > 200 or i.height > 200:
+            new_size = (200, 200)
+            i.thumbnail(new_size)
+            i.save(image_path)
