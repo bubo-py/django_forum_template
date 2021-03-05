ff5b5319125030bc0672b05dbb80a82b564b1a86
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-05 16:41:09 +0100

    added new forms for updating profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import UserProfile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+
+class UserUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UserProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('user_avatar', )
