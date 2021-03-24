a19da3979133ff6b218feac0f114b67222b80a92
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-24 20:30:00 +0100

    added new path for all_threads

--- forum/urls.py
+++ forum/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', ThreadListView.as_view(), name='index'),
     path('thread/<int:pk>/', ThreadDetailView.as_view(), name='detail'),
-    path('user/<str:username>/threads/', OneUserThreadsView.as_view(), name='one-threads-list'),
-    path('add/', views.add_thread, name='add')
+    path('user/<str:username>/threads/', OneUserThreadsView.as_view(), name='ones_threads_list'),
+    path('add/', views.add_thread, name='add'),
+    path('all-threads/', views.AllThreadListView.as_view(), name='all_threads')
 ]
