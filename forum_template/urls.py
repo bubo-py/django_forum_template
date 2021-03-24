9f7be3287fba92ddf0f2436fa02e6a6a46d5dc3a
bubo-py <kamil.chruscinski1@gmail.com>
2021-03-24 19:55:16 +0100

    formatted urlpatterns

--- forum_template/urls.py
+++ forum_template/urls.py
@@ -25,11 +25,25 @@ urlpatterns = [
     path('', include('forum.urls')),
     path('profile/', users_views.profile, name='profile'),
     path('register/', users_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm')
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'),
+        name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'),
+        name='logout'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'),
+        name='reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'),
+        name='password_reset_done'),
+
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm')
 ]
 
 
